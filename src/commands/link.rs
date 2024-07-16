acf391795061900ef13dd9a99bead53b1cf605e3
Thumus <thumus@tutanota.com>
2024-07-17 01:35:39 +0200

    add: typst-project
    It looks like it's compiling but idk there is so much stuff not directly handle rn

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -1,11 +1,13 @@
 use owo_colors::OwoColorize;
 use std::fs;
+use typst_project::manifest::Manifest;
 
 use crate::utils::{
     copy_dir_all,
     paths::{check_path_dir, d_packages, get_current_dir},
+    specs::Extra,
     state::{Error, ErrorKind, Result},
-    symlink_all, specs::{Extra, TypstConfig},
+    symlink_all,
 };
 
 use super::LinkArgs;
@@ -13,12 +15,17 @@ use super::LinkArgs;
 pub fn run(cmd: &LinkArgs, path: Option<String>) -> Result<bool> {
     let curr = path.unwrap_or(get_current_dir()?);
 
-    let config = TypstConfig::load(&(curr.clone() + "/typst.toml"));
-    let namespace = config
-        .utpm
-        .unwrap_or(Extra::default())
-        .namespace
-        .unwrap_or("local".into());
+    let config = Manifest::try_find(&(curr.clone() + "/typst.toml")).unwrap().unwrap();
+    let namespace = if let Some(value) = config.tool {
+        value
+            .get_section("utpm")
+            .unwrap()
+            .unwrap_or(Extra::default())
+            .namespace
+            .unwrap_or("local".into())
+    } else {
+        "local".into()
+    };
 
     let name = config.package.name;
     let version = config.package.version;
@@ -27,7 +34,7 @@ pub fn run(cmd: &LinkArgs, path: Option<String>) -> Result<bool> {
     let info = binding.bold();
     if check_path_dir(&path) && !cmd.force {
         return Err(Error::empty(ErrorKind::AlreadyExist(
-            name,
+            name.into(),
             version,
             format!("{}", info),
         )));
@@ -45,7 +52,6 @@ pub fn run(cmd: &LinkArgs, path: Option<String>) -> Result<bool> {
             "Project linked to: {} \nTry importing with:\n #import \"@{}/{}:{}\": *",
             path, namespace, name, version
         );
-
     } else {
         copy_dir_all(&curr, &path)?;
         println!(
