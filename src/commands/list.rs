e8a0ea7d2c8a6650e48a669dcfa353d4977c8a49
Thumus <thumus@tutanota.com>
2025-07-14 22:31:50 +0200

    refactor(list): enable usage with all output formats except plain text

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use fmt_derive::Display;
+use serde::Serialize;
 use tracing::instrument;
 
 use crate::{utils::{
@@ -6,77 +8,116 @@ use crate::{utils::{
     state::Result,
 }, utpm_println};
 
-//TODO: Faire une structure json
+
+#[derive(Serialize, Display)]
+struct Data {
+    list_namespace: Vec<Namespace> 
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Self {
+            list_namespace: vec![]
+        }
+    }
+
+}
+
+#[derive(Serialize, Display)]
+struct Namespace {
+    name: String,
+    list_packages: Vec<Package>
+}
+
+impl Namespace {
+    pub fn new(name: String) -> Self {
+        Self {
+            list_packages: vec![],
+            name
+        }
+    }
+}
+
+
+
+#[derive(Serialize, Display)]
+struct Package {
+    name: String,
+    list_version: Vec<String>
+}
+
+impl Package {
+    pub fn new(name: String) -> Self {
+        Self {
+            list_version: vec![],
+            name
+        }
+    }
+}
+
 
 use super::ListTreeArgs;
 
 #[instrument(skip(cmd))]
 pub fn run(cmd: &ListTreeArgs) -> Result<bool> {
     let typ: String = d_packages()?;
-    utpm_println!("A list of your packages (WIP)\n");
     if cmd.all {
         let preview: String = c_packages()?;
-        read(typ)?;
-        return read(preview);
+        let data1 = read(typ)?;
+        let data2 = read(preview)?;
+        utpm_println!(data1);
+        utpm_println!(data2);
+        return Ok(true)
     }
 
     if let Some(list) = &cmd.include {
         let preview: String = c_packages()?;
         for e in list {
             if e == "preview" {
-                return read(preview);
+                let data = read(preview)?;
+                utpm_println!(data);
+                return Ok(true);
             }
-            match package_read(&format!("{}/local/{}", typ, e)) {
-                Ok(_) => true,
-                Err(_) => namespace_read(&format!("{}/{}", typ, e))?,
+            match package_read(&format!("{}/local/{}", typ, e), e.to_string()) {
+                Err(_)=> {namespace_read(&format!("{}/{}",typ,e), e.to_string())?;} ,
+                Ok(_) => {},
             };
         }
         Ok(true)
     } else {
-        read(typ)
+        let data = read(typ)?;
+        utpm_println!(data);
+        return Ok(true)
     }
 }
 
-fn read(typ: String) -> Result<bool> {
+fn read(typ: String) -> Result<Data> {
     let dirs = fs::read_dir(&typ)?;
+    let mut data = Data::new();
 
     for dir_res in dirs {
         let dir = dir_res?;
-        utpm_println!("@{}: ", dir.file_name().to_str().unwrap());
-        let subupdirs = fs::read_dir(dir.path())?;
-
-        for dir_res in subupdirs {
-            let dir = dir_res?;
-            utpm_println!("{}: ", dir.file_name().to_str().unwrap());
-
-            let subdirs = fs::read_dir(dir.path())?;
-            for sub_dir_res in subdirs {
-                let subdir = sub_dir_res?;
-                utpm_println!("{} ", subdir.file_name().to_str().unwrap());
-            }
-        }
+        let nms = namespace_read(&dir.path().to_str().unwrap().into(), dir.file_name().into_string().unwrap())?;
+        data.list_namespace.push(nms);
     }
-    Ok(true)
+    Ok(data)
 }
 
-fn package_read(typ: &String) -> Result<bool> {
+fn package_read(typ: &String, name: String) -> Result<Package> {
+    let mut pkg = Package::new(name);
     for dir_res in fs::read_dir(&typ)? {
-        let dir = dir_res?;
-        utpm_println!("{}: ", dir.file_name().to_str().unwrap());
+        let dir: fs::DirEntry = dir_res?;
+        pkg.list_version.push(dir.file_name().to_str().unwrap().into());
     }
-    println!();
-    Ok(true)
+    Ok(pkg)
 }
 
-fn namespace_read(typ: &String) -> Result<bool> {
+fn namespace_read(typ: &String, name: String) -> Result<Namespace> {
+    let mut nms = Namespace::new(name);
     for dir_res in fs::read_dir(&typ)? {
         let dir = dir_res?;
-        utpm_println!("{}: ", dir.file_name().to_str().unwrap());
-        utpm_println!("- ");
-        for dir_res in fs::read_dir(dir.path())? {
-            let dir = dir_res?;
-            utpm_println!("{} ", dir.file_name().to_str().unwrap());
-        }
+        let pkg = package_read(&dir.path().to_str().unwrap().into(), dir.file_name().to_str().unwrap().into())?;
+        nms.list_packages.push(pkg);
     }
-    Ok(true)
+    Ok(nms)
 }
