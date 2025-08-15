8ee87fe716b162859c3210f2800f3fba02deba40
Clemens Koza <clemens.koza@gmx.at>
2025-08-15 14:12:09 +0200

    use toml_edit to preserve manifest formatting

--- src/commands/bump.rs
+++ src/commands/bump.rs
@@ -1,19 +1,19 @@
 use std::borrow::Cow;
-use std::fs::{read_to_string, write};
+use std::io::Write;
+use std::fs::{File, read_to_string, write};
 use std::str::FromStr;
 
 use itertools::Itertools;
+use toml_edit::{DocumentMut, value};
 use tracing::instrument;
 use typst_syntax::package::PackageVersion;
 
 use crate::{
-    utils::{dryrun::get_dry_run, state::Result},
-    utpm_log, write_manifest,
+    utils::{self, dryrun::get_dry_run, state::Result},
+    utpm_log,
 };
 
 use super::BumpArgs;
-
-use crate::load_manifest;
 
 /// Check if there is a tag.
 /// If there is, it creates a new string like an html element
@@ -29,18 +29,19 @@ fn tag_change<'a>(tag: Option<&str>, value: &'a str) -> Cow<'a, str> {
 #[instrument(skip(cmd))]
 pub async fn run<'a>(cmd: &'a BumpArgs) -> Result<bool> {
     utpm_log!(trace, "executing bump command");
-    let mut config = load_manifest!();
+    let manifest_path = utils::try_find_path(&utils::paths::get_current_dir()?)?;
+    let mut config = read_to_string(&manifest_path)?.parse::<DocumentMut>()?;
 
-    let old_version = config.package.version.to_string();
+    let old_version = config["package"]["version"].as_str().unwrap();
     let new_version = cmd.new_version.as_str();
-    let ver = PackageVersion::from_str(new_version).unwrap();
+    PackageVersion::from_str(new_version).unwrap();
 
     let files = &cmd.include;
 
     for file in files {
         let mut string = read_to_string(file)?;
         utpm_log!(info, "Found {}", file);
-        let old_version = tag_change(cmd.tag.as_deref(), &old_version);
+        let old_version = tag_change(cmd.tag.as_deref(), old_version);
         let new_version = tag_change(cmd.tag.as_deref(), new_version);
         string = string.replace(old_version.as_ref(), &new_version);
         if !get_dry_run() {
@@ -54,8 +55,11 @@ pub async fn run<'a>(cmd: &'a BumpArgs) -> Result<bool> {
             .chain(files.iter().map(AsRef::<str>::as_ref))
             .join(", ");
 
-    config.package.version = ver;
-    write_manifest!(&config);
+    config["package"]["version"] = value(new_version);
+    let mut file = File::create(manifest_path)?;
+    write!(file, "{}", config)?;
+    file.sync_all()?;
+
     utpm_log!(info, format!("New version: {new_version}"), "version" => new_version, "files" => files);
     Ok(true)
 }
