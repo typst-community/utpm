58e56674bb0d9e381594918594f20bf5ad557a86
Thomas QUEMIN Thumuss <me@thumus.eu>
2025-08-17 03:50:10 +0200

    fix(state): remove unused errors

--- src/utils/state.rs
+++ src/utils/state.rs
@@ -36,14 +36,6 @@ pub enum UtpmError {
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
 
-    /// An error indicating that a git rebase is required but not yet supported.
-    #[error("We can't rebase (for now)")]
-    Rebase,
-
-    /// A general-purpose error.
-    #[error("General error: {0}")]
-    General(String),
-
     /// An error during JSON serialization or deserialization.
     #[cfg(feature = "output_json")]
     #[error("Can't parse to json: {0}")]
@@ -58,14 +50,6 @@ pub enum UtpmError {
     #[cfg(feature = "output_yaml")]
     #[error("Can't parse to yaml: {0}")]
     YamlParse(#[from] serde_yaml::Error),
-
-    /// An error parsing an email address.
-    #[error("Email parse error: {0}")]
-    Email(String),
-
-    /// An error parsing a GitHub handle.
-    #[error("GitHub handle parse error: {0}")]
-    GithubHandle(String),
 
     /// An error during TOML serialization.
     #[error("TOML serialization error: {0:?}")]
@@ -93,21 +77,9 @@ pub enum UtpmError {
     #[error("Unknown error: {0}")]
     Unknown(String),
 
-    /// An error for a missing namespace or package name.
-    #[error("Missing namespace or package name.")]
-    Namespace,
-
-    /// An error for a missing configuration file.
-    #[error("Missing configuration file.")]
-    ConfigFile,
-
     /// An error when the current directory cannot be determined.
     #[error("Couldn't find the current directory.")]
     CurrentDir,
-
-    /// An error when a directory fails to be created.
-    #[error("Failed to create directory.")]
-    CreationDir,
 
     /// An error when the home directory cannot be determined.
     #[error("Could not determine home directory.")]
@@ -116,10 +88,6 @@ pub enum UtpmError {
     /// An error for a missing `typst.toml` manifest.
     #[error("Missing manifest.")]
     Manifest,
-
-    /// An error for not enough arguments provided to a command.
-    #[error("Not enough arguments provided.")]
-    NotEnoughArgs,
 
     /// An error for an invalid package string.
     #[error("Can't extract your package. Example of a package: @namespace/package:1.0.0")]
@@ -152,9 +120,6 @@ pub enum UtpmError {
     #[error("{2} Package {0} with version {1} already exist.")]
     AlreadyExist(String, PackageVersion, String),
 
-    /// An error when no URIs are provided for a command that requires them.
-    #[error("No URI were found. Please check your typst.toml")]
-    NoURIFound,
 
     #[error("Reqwest Error: {0}")]
     ReqwestError(#[from] reqwest::Error),
@@ -192,9 +157,6 @@ impl UtpmError {
             #[cfg(any(feature = "init", feature = "unlink"))]
             Questions(_) => "Questions",
             IO(_) => "IO",
-            General(_) => "General",
-            Email(_) => "Email",
-            GithubHandle(_) => "GithubHandle",
             Serialize(_) => "Serialize",
             Deserialize(_) => "Deserialize",
             DeserializeMut(_) => "DeserializeMut",
@@ -203,25 +165,19 @@ impl UtpmError {
             #[cfg(any(feature = "publish"))]
             OctoCrab(_) => "OctoCrab",
             Unknown(_) => "Unknown",
-            Namespace => "Namespace",
-            ConfigFile => "ConfigFile",
             CurrentDir => "CurrentDir",
-            CreationDir => "CreationDir",
             HomeDir => "HomeDir",
             Manifest => "Manifest",
-            NotEnoughArgs => "NotEnoughArgs",
             PackageNotValid => "PackageNotValid",
             PackageNotExist => "PackageNotExist",
             ContentFound => "ContentFound",
             AlreadyExist(_, _, _) => "AlreadyExist",
             Other(_) => "Other",
-            Rebase => "Rebase",
             JsonParse(_) => "JSONParse",
             #[cfg(feature = "output_hjson")]
             HJsonParse(_) => "HJSONParse",
             #[cfg(feature = "output_yaml")]
             YamlParse(_) => "YamlParse",
-            NoURIFound => "NoURIFound",
             ReqwestError(_) => "ReqwestError",
             FromUTF8Error(_) => "FromUTF8Error",
             GitNotFound => "GitNotFound",
